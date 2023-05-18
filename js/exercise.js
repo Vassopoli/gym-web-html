eeb1b9e1c1553068a07ea722b9b19b1806c819f3
Maiza Oliveira Amaral <maiza.oliveira54@gmail.com>
2023-05-17 22:29:08 -0300

    add load type + adjust details

--- js/exercise.js
+++ js/exercise.js
@@ -6,22 +6,24 @@ function loadExercise() {
     document.getElementById("exerciseName").innerHTML = exerciseReal.primaryName;
 
     if (exerciseReal.workoutExercises.length > 1) {
-        document.getElementById("lastTime").innerHTML = exerciseReal.workoutExercises[1].sets + "x" + exerciseReal.workoutExercises[1].repetitions + " with " + exerciseReal.workoutExercises[1].loadValue;
+        document.getElementById("lastTime").innerHTML = exerciseReal.workoutExercises[1].sets + "x" 
+            + exerciseReal.workoutExercises[1].repetitions + " with " 
+            + exerciseReal.workoutExercises[1].loadValue + " " + exerciseReal.workoutExercises[1].loadType.name;
     }
-
+ 
     document.getElementById('videoTutorial').src = "https://www.youtube.com/embed/" + exerciseReal.idYoutubeVideo;
     document.getElementById('muscleName').innerHTML = exerciseReal.muscle.name;
     document.getElementById('exerciseDetails').innerHTML = exerciseReal.workoutExercises[0].sets + "x" + exerciseReal.workoutExercises[0].repetitions;
-    document.getElementById('rest').innerHTML = "Rest: " + exerciseReal.workoutExercises[0].restSeconds + "sec";
+    document.getElementById('rest').innerHTML = exerciseReal.workoutExercises[0].restSeconds + "sec";
 
     if (exerciseReal.workoutExercises[0].loadValue != null) {
         document.getElementById('exerciseDetails').innerHTML = document.getElementById('exerciseDetails').innerHTML
-        + " with " + exerciseReal.workoutExercises[0].loadValue
+        + " with " + exerciseReal.workoutExercises[0].loadValue + " " + exerciseReal.workoutExercises[0].loadType.name;
     }
 
     if (exerciseReal.workoutExercises[0].advancedTechnique.name != null) {
         document.getElementById('exerciseDetails').innerHTML = document.getElementById('exerciseDetails').innerHTML
-        + "<br>" + exerciseReal.workoutExercises[0].advancedTechnique.name
+        + "<br>" + exerciseReal.workoutExercises[0].advancedTechnique.name;
     }
 }
 
